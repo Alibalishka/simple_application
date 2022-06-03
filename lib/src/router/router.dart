b16842de20c2876172a8a28f95c7f9d95f4ce141
Alibalishka <groznyi.alik@gmail.com>
2022-06-03 20:26:42 +0600

    Add routers between screens

---
+++ lib/src/router/router.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_application_1/src/router/routing_const.dart';
+import 'package:flutter_application_1/src/screens/auth/auth_screen.dart';
+import 'package:flutter_application_1/src/screens/register/reg_screen.dart';
+
+class AppRouter{
+  static Route generateRoute(RouteSettings routeSettings){
+    switch(routeSettings.name){
+      case AuthRoute:
+        return CupertinoPageRoute(
+          builder: (context) => AuthScreen(),
+        );
+      case RegisterRoute:
+        return CupertinoPageRoute(
+          builder: (context) => Register(),
+        );
+      default:
+        return CupertinoPageRoute(
+          builder: (context) => AuthScreen(),
+        );
+    }
+  }
+}
