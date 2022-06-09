1fe2d2d929740d4451bde9557b74560e9d009413
Alibalishka <groznyi.alik@gmail.com>
2022-06-09 23:23:10 +0600

    add routes DetailScreen & Map Screen

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_application_1/src/router/routing_const.dart';
 import 'package:flutter_application_1/src/screens/auth/auth_screen.dart';
+import 'package:flutter_application_1/src/screens/detail/detail_screen.dart';
 import 'package:flutter_application_1/src/screens/mainScreen/main_screen.dart';
+import 'package:flutter_application_1/src/screens/map/map_screen.dart';
 import 'package:flutter_application_1/src/screens/register/reg_screen.dart';
 
 class AppRouter{
@@ -18,6 +20,14 @@ class AppRouter{
       case MainRoute:
         return CupertinoPageRoute(
           builder: (context) => MainScreen(),
+        );
+      case RestaurantDetailRoute:
+        return CupertinoPageRoute(
+          builder: (context) => DetailScreen(),
+        );
+      case MapRoute:
+        return CupertinoPageRoute(
+          builder: (context) => MapScreen(),
         );
       default:
         return CupertinoPageRoute(
