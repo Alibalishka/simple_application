9a7b362d7b1eee463a31f6ba817bc9f029e08cb4
Alibalishka <groznyi.alik@gmail.com>
2022-06-12 16:34:57 +0600

    create Map Screen

--- lib/src/screens/map/map_screen.dart
+++ lib/src/screens/map/map_screen.dart
@@ -1,23 +1,40 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_application_1/src/common/widgets/custom_colors.dart';
+import 'dart:async';
 
-class MapScreen extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/common/widgets/custom_colors.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+
+  final Completer<GoogleMapController> _controller = Completer();
+  static const CameraPosition initialPosition = CameraPosition(target: LatLng(37.15478, -122.78945), zoom: 14.0);
+  static const CameraPosition targetPosition = CameraPosition(target: LatLng(33.15478, -135.78945), zoom: 14.0, bearing: 192.0, tilt: 60); 
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       backgroundColor: AppColors.scaffoldBackground,
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Map'),
-      ),
       child: SafeArea(
-        child: Column(
-          children: [
-            
-          ],
+        child: GoogleMap(
+          initialCameraPosition: initialPosition,
+          mapType: MapType.normal,
+          onMapCreated: (GoogleMapController controller){
+            _controller.complete(controller);
+          },
         ),
       ),
     );
+  }
+  Future<void> goToLake() async{
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(targetPosition));
   }
 }
