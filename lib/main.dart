90051398562bc3d69d6b823218d84dd21f1e49bf
Alibalishka <groznyi.alik@gmail.com>
2022-06-17 17:36:50 +0600

    create initialRoute and constant back. color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_application_1/src/common/widgets/custom_colors.dart';
 import 'package:flutter_application_1/src/router/router.dart';
+import 'package:flutter_application_1/src/router/routing_const.dart';
 import 'package:flutter_application_1/src/screens/auth/auth_screen.dart';
 
 
@@ -13,7 +15,11 @@ class MyApp extends StatelessWidget {
     return CupertinoApp(
       debugShowCheckedModeBanner: false,
       onGenerateRoute: AppRouter.generateRoute,
-      home: AuthScreen(),
+      initialRoute: AuthRoute,
+      theme: CupertinoThemeData(
+        scaffoldBackgroundColor: AppColors.scaffoldBackground,
+        barBackgroundColor: AppColors.white,
+      ),
     );
   }
 }
