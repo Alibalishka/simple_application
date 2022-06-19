1a134cc0f5214a528bc6249b5f73237028a411eb
Alibalishka <groznyi.alik@gmail.com>
2022-06-19 16:04:52 +0600

    add method initState check tokens

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,45 @@ import 'package:flutter_application_1/src/common/widgets/custom_colors.dart';
 import 'package:flutter_application_1/src/router/router.dart';
 import 'package:flutter_application_1/src/router/routing_const.dart';
 import 'package:flutter_application_1/src/screens/auth/auth_screen.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  // Создаем коробку tokens что-бы сохранить токены 
+  await Hive.openBox('tokens');
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Создаём переменную initialRoute, которую закидываем в CupertinoApp, и задаём изначальное значение AuthRoute
+  String initialRoute = AuthRoute;
+  
+  @override
+  void initState(){
+    // Получаем Box токенов
+    Box tokensBox = Hive.box('tokens');
+
+    // Делаем проверку если access или refresh токены равны 'null'
+    if(tokensBox.get('access') != null || tokensBox.get('refresh') != null){
+      // Если пользователь авторизован, то меняем initialRoute на MainRoute
+      initialRoute = MainRoute;
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
       debugShowCheckedModeBanner: false,
       onGenerateRoute: AppRouter.generateRoute,
-      initialRoute: AuthRoute,
+      initialRoute: initialRoute,
       theme: CupertinoThemeData(
         scaffoldBackgroundColor: AppColors.scaffoldBackground,
         barBackgroundColor: AppColors.white,
