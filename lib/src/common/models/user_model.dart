13c87d3f6cfcdc932100420622191721c24ab8d6
Alibalishka <groznyi.alik@gmail.com>
2022-06-20 14:21:47 +0600

    create User Model

---
+++ lib/src/common/models/user_model.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_model.g.dart';
+
+@JsonSerializable()
+class UserModel{
+  final int? id;
+  final String? email;
+  final String? nickname;
+
+  UserModel({
+    required this.id,
+    required this.email,
+    required this.nickname,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => _$UserModelFromJson(json);
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
+}
